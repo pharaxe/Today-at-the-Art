d23d323130330cad88d792c8ffa9c7495c0326e2
Benjamin Sweedler <bsweedler@pavlovmedia.com>
2019-09-28 14:38:36 -0500

    imagery!

---
+++ src/app/chairs/chairs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chairs',
+  templateUrl: './chairs.component.html',
+  styleUrls: ['./chairs.component.scss']
+})
+export class ChairsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
