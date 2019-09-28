5d5dc65c47995aeb29574b432dd45a6955dbf27d
Justin White <kyjus25@gmail.com>
2019-09-27 22:27:30 -0500

    Merge showtime to month chart

--- src/app/diagnosis/diagnosis.component.ts
+++ src/app/diagnosis/diagnosis.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {ConcessionMetric} from '../types/concession.type';
+import {ShowMetric} from '../types/showtime.type';
+
+const showtimes: ShowMetric[] = require('../../assets/datasets/json/ticket-sales-by-showtime.json');
+const concessions: ConcessionMetric[] = require('../../assets/datasets/json/concessions.json');
 
 @Component({
   selector: 'app-diagnosis',
@@ -7,26 +12,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiagnosisComponent implements OnInit {
 
-  public data: any;
+  public shows: string[];
+  public data;
+
+  private months: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+    'August', 'September', 'October', 'November', 'December'];
 
   constructor() {
+
+    this.shows = [...new Set(showtimes.map(obj => obj.EventName))];
+
+    console.log(concessions);
+
+    const showDatesMap = {};
+    showtimes.forEach(show => {
+      const month = new Date(show.EventDate).getMonth();
+      showDatesMap[month] ? showDatesMap[month] += show.Quantity : showDatesMap[month] = show.Quantity;
+      // console.log(showDatesMap);
+    });
+
+    const concessionDatesMap = {};
+    concessions.forEach(concession => {
+      const month = new Date(concession['Order Date']).getMonth();
+      concessionDatesMap[month] ? concessionDatesMap[month] += concession.Quantity : concessionDatesMap[month] = concession.Quantity;
+      // console.log(concessionDatesMap);
+    });
+
     this.data = {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      labels: this.months,
       datasets: [
         {
-            label: 'My First dataset',
+            label: 'Showtime Sales',
             backgroundColor: '#42A5F5',
             borderColor: '#1E88E5',
-            data: [65, 59, 80, 81, 56, 55, 40]
+            fill: false,
+            data: [
+              showDatesMap[0],
+              showDatesMap[1],
+              showDatesMap[2],
+              showDatesMap[3],
+              showDatesMap[4],
+              showDatesMap[5],
+              showDatesMap[6],
+              showDatesMap[7],
+              showDatesMap[8],
+              showDatesMap[9],
+              showDatesMap[10],
+              showDatesMap[11]
+            ]
         },
         {
-            label: 'My Second dataset',
+            label: 'Concession Sales',
             backgroundColor: '#9CCC65',
             borderColor: '#7CB342',
-            data: [28, 48, 40, 19, 86, 27, 90]
+            fill: false,
+            data: [
+              concessionDatesMap[0],
+              concessionDatesMap[1],
+              concessionDatesMap[2],
+              concessionDatesMap[3],
+              concessionDatesMap[4],
+              concessionDatesMap[5],
+              concessionDatesMap[6],
+              concessionDatesMap[7],
+              concessionDatesMap[8],
+              concessionDatesMap[9],
+              concessionDatesMap[10],
+              concessionDatesMap[11]
+            ]
         }
       ]
-    }
+    };
   }
 
   ngOnInit() {
---
+++ src/app/types/showtime.type.ts
@@ -0,0 +1,10 @@
+export interface ShowMetric {
+  EventDate: string;
+  EventName: string;
+  Folder: string;
+  InternalEventName: string;
+  ItemPrice: number;
+  Price: number;
+  Quantity: number;
+  TicketType: string;
+}
